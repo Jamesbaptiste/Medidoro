74becb9069d18997331d1a60013f5648e8535bc6
James Baptiste <Jamesbaptiste@lewisu.edu>
2020-03-06 06:36:26 -0600

    2nd commit

--- app.js
+++ app.js
@@ -1,61 +1,63 @@
 const app = () => {
-  const song = document.querySelector(".song");
+  const audio = document.querySelector(".audio");
+  const video = document.querySelector(".video-container video");
   const play = document.querySelector(".play-button");
-  const video = document.querySelector(".vid-container video");
+
   //Sounds
-  const sounds = document.querySelectorAll(".sound-picker button");
+  const sounds = document.querySelectorAll(".sounds button");
   //time display
   const timeDisplay = document.querySelector(".time-display");
-  const timeSelect = document.querySelectorAll(".time-select button");
+  //choose minutes
+  const chooseMinutes = document.querySelectorAll(".minutes button");
 
   //duration
-  let fakeDuration = 1500;
-  //pick differnt sound/video change
+  let Duration = 1500;
+  //pick different sound/video change
   sounds.forEach(sound => {
     sound.addEventListener("click", function() {
-      song.src = this.getAttribute("data-sound");
+      audio.src = this.getAttribute("data-sound");
       video.src = this.getAttribute("data-video");
-      checkPlaying(song);
+      Playing(audio);
     });
   });
-  //play sound
+  //play audio
   play.addEventListener("click", () => {
-    checkPlaying(song);
+    Playing(audio);
   });
   //play and pause
-  const checkPlaying = song => {
-    if (song.paused) {
-      song.play();
+  const Playing = audio => {
+    if (audio.paused) {
+      audio.play();
       video.play();
       play.innerHTML = "Pause";
     } else {
-      song.pause();
+      audio.pause();
       video.pause();
       play.innerHTML = "Play";
     }
   };
   //select sound
-  timeSelect.forEach(option => {
+  chooseMinutes.forEach(option => {
     option.addEventListener("click", function() {
-      fakeDuration = this.getAttribute("data-time");
-      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-        fakeDuration % 60
+      Duration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(Duration / 60)}:${Math.floor(
+        Duration % 60
       )}`;
     });
   });
-  song.ontimeupdate = () => {
-    let currentTime = song.currentTime;
-    let elapsed = fakeDuration - currentTime;
+  audio.ontimeupdate = () => {
+    let currentTime = audio.currentTime;
+    let elapsed = Duration - currentTime;
     let seconds = Math.floor(elapsed % 60);
     let minutes = Math.floor(elapsed / 60);
 
     //animate text
     timeDisplay.textContent = `${minutes}:${seconds}`;
     //change audio vid at end
-    if (currentTime >= fakeDuration) {
-      song.pause();
+    if (currentTime >= Duration) {
+      audio.pause();
       video.pause();
-      song.currentTime = 0;
+      audio.currentTime = 0;
     }
   };
 };
